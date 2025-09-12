ea1b50ec0bdfc1f8feee61844b0dacb9be5e6caa
nzsombor04 <nzsombor04@gmail.hu>
2025-09-12 12:36:23 +0200

    Test for frontend done, cors needs to be handled on backend

--- Frontend/eduShare/src/app/app.module.ts
+++ Frontend/eduShare/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestComponent } from './components/test/test.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
