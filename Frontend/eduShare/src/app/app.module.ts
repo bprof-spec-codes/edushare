284d042c06a70cc27926a4bdde6c52a09f1ea854
batoriandras <batori.andras@gmail.com>
2025-10-02 19:32:14 +0200

    Add routing for material creation and implement MaterialCreateComponent; moved Navbar component

--- Frontend/eduShare/src/app/app.module.ts
+++ Frontend/eduShare/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { TestComponent } from './components/test/test.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MaterialCreateComponent } from './components/material-create/material-create.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TestComponent,
-    NavbarComponent
+    NavbarComponent,
+    MaterialCreateComponent
   ],
   imports: [
     BrowserModule,
---
+++ Frontend/eduShare/src/app/components/material-create/material-create.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-material-create',
+  standalone: false,
+  templateUrl: './material-create.component.html',
+  styleUrl: './material-create.component.sass'
+})
+export class MaterialCreateComponent {
+
+}
