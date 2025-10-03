a9c541086f5f361226148b0e8044a46adc3a06a5
Keserű Márton <gergelymarton00@gmail.com>
2025-10-03 15:59:14 +0200

    DtoProvider

---
+++ Backend/EdushareBackend/Logic/Helper/DtoProviders.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Entities.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Metadata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Helper
+{
+    public class DtoProviders 
+    {
+        UserManager<AppUser> userManager;
+        public Mapper Mapper { get; set; }
+    }
+}
---
+++ Backend/EdushareBackend/Logic/Logic/MaterialLogic.cs
@@ -0,0 +1,51 @@
+﻿using Data;
+using Entities.Dtos.Material;
+using Entities.Models;
+using Logic.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Logic
+{
+    public class MaterialLogic
+    {
+         Repository<Material> repo;
+        DtoProviders dtoProviders;
+        
+        public MaterialLogic(Repository<Material> repo, DtoProviders dtoProviders)
+        {
+            this.repo = repo;
+            this.dtoProviders = dtoProviders;
+        }
+
+        public void AddMaterial(MaterialCreateUpdateDto material)
+        {
+            Material mat=dtoProviders.Mapper.Map<Material>(material);
+            repo.Add(mat);
+            
+        }
+        public IEnumerable<MaterialShortViewDto> GetAllMaterials()
+        {
+            return repo.GetAll().Select(x => dtoProviders.Mapper.Map<MaterialShortViewDto>(x));
+        }
+        public void DeleteMaterialById(string id)
+        {
+            repo.DeleteById(id);
+        }
+        public void UpdateMaterial(string id, MaterialCreateUpdateDto material)
+        {
+            var old= repo.FindById(id);
+            var mat=dtoProviders.Mapper.Map(material,old);
+            repo.Update(mat);
+        }
+        public MaterialViewDto GetMaterialById(string id)
+        {
+            var mat= repo.FindById(id);
+            return dtoProviders.Mapper.Map<MaterialViewDto>(mat);
+        }
+
+    }
+}
