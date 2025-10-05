e78d6a9fa14ef2142475a00f2990114e0ca27fbf
batoriandras <batori.andras@gmail.com>
2025-10-05 20:39:09 +0200

    Implement MaterialList component with loading and error handling; update routing to include MaterialList

--- Frontend/eduShare/src/app/app-routing.module.ts
+++ Frontend/eduShare/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MaterialCreateComponent } from './components/material-create/material-create.component';
+import { MaterialListComponent } from './components/material-list/material-list.component';
 
 const routes: Routes = [
   {path:"",redirectTo: "create-material",pathMatch:"full"},
   {path:"create-material",component: MaterialCreateComponent},
+  {path:"list-materials",component: MaterialListComponent},
   {path:"**",redirectTo:"create-material",pathMatch:"full"}
 ];
 
