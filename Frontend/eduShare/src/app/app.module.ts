039671cad81519f77776aa15a3a1191d3a8156bf
hanuszbettina <hanuszbettina.sulis@gmail.com>
2025-10-05 22:00:40 +0200

    register card created

--- Frontend/eduShare/src/app/app.module.ts
+++ Frontend/eduShare/src/app/app.module.ts
@@ -8,12 +8,14 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { MaterialCreateComponent } from './components/material-create/material-create.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    MaterialCreateComponent
+    MaterialCreateComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
