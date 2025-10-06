563a8067bd5c6aa3a891fba291f046b79dc8968d
hanuszbettina <hanuszbettina.sulis@gmail.com>
2025-10-06 09:31:02 +0200

    Required fields ready

--- Frontend/eduShare/src/app/components/register/register.component.ts
+++ Frontend/eduShare/src/app/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators  } from '@angular/forms';
+
 
 @Component({
   selector: 'app-register',
@@ -8,4 +10,16 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
   hidePass:boolean=true
+  registerForm: FormGroup
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group(
+      {
+        lastname: ['', [Validators.required, Validators.minLength(3)]],
+        firstname: ['', [Validators.required, Validators.minLength(3)]],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(8)]],
+        password2: ['', [Validators.required, Validators.minLength(8)]]
+      }
+    )
+  }
 }
