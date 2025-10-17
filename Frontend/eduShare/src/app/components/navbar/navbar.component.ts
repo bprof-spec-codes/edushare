6e1cc875e19ac00bfb52499086092393836ba056
batoriandras <batori.andras@gmail.com>
2025-10-17 20:04:30 +0200

    Refactor navbar component layout and add user ID retrieval in AuthService

--- Frontend/eduShare/src/app/components/navbar/navbar.component.ts
+++ Frontend/eduShare/src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnChanges, OnInit } from '@angular/core';
 import { AuthService } from '../../services/authentication.service';
+import { MaterialService } from '../../services/material.service';
 
 @Component({
   selector: 'app-navbar',
@@ -10,8 +11,9 @@ import { AuthService } from '../../services/authentication.service';
 export class NavbarComponent implements OnChanges {
   isLoggedIn: boolean = false;
 
-  constructor(private auth: AuthService) {
+  constructor(private auth: AuthService, private materialService: MaterialService) {
     this.isLoggedIn = this.auth.isLoggedIn();
+    console.log('userid: ', auth.getUserId());
   }
 
   ngOnChanges() {
