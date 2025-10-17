6dcb98f137b15289be38a3c8d57427ac4c1eb218
batoriandras <batori.andras@gmail.com>
2025-10-17 18:41:59 +0200

    Refactor navbar component: update layout, add logo, and implement login state management

--- Frontend/eduShare/src/app/components/navbar/navbar.component.ts
+++ Frontend/eduShare/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnInit } from '@angular/core';
 import { AuthService } from '../../services/authentication.service';
 
 @Component({
@@ -7,8 +7,16 @@ import { AuthService } from '../../services/authentication.service';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.sass'
 })
-export class NavbarComponent {
-  constructor(private auth: AuthService) {}
+export class NavbarComponent implements OnChanges {
+  isLoggedIn: boolean = false;
+
+  constructor(private auth: AuthService) {
+    this.isLoggedIn = this.auth.isLoggedIn();
+  }
+
+  ngOnChanges() {
+    this.isLoggedIn = this.auth.isLoggedIn();
+  }
 
   logout() {
     this.auth.logout();
