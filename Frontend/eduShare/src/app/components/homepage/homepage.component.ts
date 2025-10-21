77f99850d1b67c7405c1e922caf93ec81bb71667
hanuszbettina <hanuszbettina.sulis@gmail.com>
2025-10-21 21:05:51 +0200

    homepage design

--- Frontend/eduShare/src/app/components/homepage/homepage.component.ts
+++ Frontend/eduShare/src/app/components/homepage/homepage.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MaterialViewDto } from '../../dtos/material-view-dto';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-homepage',
@@ -7,5 +9,11 @@ import { Component } from '@angular/core';
   styleUrl: './homepage.component.sass'
 })
 export class HomepageComponent {
+  constructor(private router: Router){
 
+  }
+
+  showMaterials(): void {
+         this.router.navigate(['/materials'])
+    }
 }
