ab18706d8714fe7241ebc90d72176739a934d849
hanuszbettina <hanuszbettina.sulis@gmail.com>
2025-11-04 20:05:25 +0100

    material-list component has a recommended section

--- Frontend/eduShare/src/app/components/material-list/material-list.component.ts
+++ Frontend/eduShare/src/app/components/material-list/material-list.component.ts
@@ -11,12 +11,14 @@ import { Router } from '@angular/router';
 })
 export class MaterialListComponent {
   materials: MaterialShortViewDto[] = []
+  recommendedMaterials: MaterialShortViewDto[] = []
   loading = false
   error?: string
 
   constructor(private materialService: MaterialService, private router: Router) { }
   ngOnInit(): void {
     this.loadMaterials()
+    
   }
 
   loadMaterials(): void {
@@ -24,6 +26,7 @@ export class MaterialListComponent {
     this.materialService.loadAll().subscribe({
       next: (data) => {
         this.materials = data
+        this.recommendedMaterials = data.filter(m => m.isRecommended)
         this.loading = false
       },
       error: (err) => {
