daf6d339b7caa6d4cb160347e5ef81b94188946e
batoriandras <batori.andras@gmail.com>
2025-11-06 17:17:47 +0100

    Add FavMaterialService to manage favorite materials and favmaterials getAll()

---
+++ Frontend/eduShare/src/app/services/fav-material.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
+import { MaterialShortViewDto } from '../dtos/material-short-view-dto';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment.development';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FavMaterialService {
+
+  private apiBaseUrl = `${environment.baseApiUrl}/api/material`
+
+  private _favMaterials$ = new BehaviorSubject<MaterialShortViewDto[]>([])
+  public favMaterials$ = this._favMaterials$.asObservable()
+
+
+
+  constructor(private http: HttpClient) {}
+
+  getAll():Observable<MaterialShortViewDto[]>{
+    return this.http.get<MaterialShortViewDto[]>(`${this.apiBaseUrl}/favouriteMaterials`).pipe(
+      tap( favMaterials =>{
+        this._favMaterials$.next(favMaterials)
+      })
+    )
+  }
+}
