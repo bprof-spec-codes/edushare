1b94a13953fb3bef981b6d31d033f1e891e4e5c5
Nagy Zsombor <nzsombor04@gmail.com>
2025-11-06 14:57:35 +0100

    uploader added

--- Frontend/eduShare/src/app/components/searchbar/searchbar.component.ts
+++ Frontend/eduShare/src/app/components/searchbar/searchbar.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { map, Observable, of } from 'rxjs';
+import { Subject } from '../../models/subject';
+import { SubjectService } from '../../services/subject.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-searchbar',
@@ -6,6 +10,26 @@ import { Component } from '@angular/core';
   templateUrl: './searchbar.component.html',
   styleUrl: './searchbar.component.sass'
 })
-export class SearchbarComponent {
+export class SearchbarComponent implements OnInit{
+  subjects$ = new Observable<Subject[]>()
+  semesters$ = new Observable<number[]>()
+  form!: FormGroup
 
+  constructor(private subjectService: SubjectService, private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      subject: [""],
+      semester: [0],
+      title: [""]
+    })
+
+    this.subjects$ = this.subjectService.getAllSubjects().pipe(
+      map(subjects => subjects.slice().sort((a, b) => a.name.localeCompare(b.name)))
+    );
+
+    const semesters: number[] = [1, 2, 3, 4, 5, 6, 7, 8]
+
+    this.semesters$ = of(semesters)
+  }
 }
