2b26c83bc82e3f7e3a90d30dadd447c62b02d09b
batoriandras <batori.andras@gmail.com>
2025-11-13 19:40:07 +0100

    feat:  RatingCardComponent - rating connected with user

--- Frontend/eduShare/src/app/components/rating-card/rating-card.component.ts
+++ Frontend/eduShare/src/app/components/rating-card/rating-card.component.ts
@@ -1,4 +1,9 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ProfileService } from '../../services/profile.service';
+import { map, Observable } from 'rxjs';
+import { ProfileViewDto } from '../../dtos/profile-view-dto';
+import { ProfilListViewDto } from '../../dtos/profil-list-view-dto';
+import { RatingViewDto } from '../../dtos/rating-view-dto';
 
 @Component({
   selector: 'app-rating-card',
@@ -6,8 +11,19 @@ import { Component, Input } from '@angular/core';
   templateUrl: './rating-card.component.html',
   styleUrl: './rating-card.component.sass'
 })
-export class RatingCardComponent {
-  @Input() userName!: string
-  @Input() rating!: number
-  @Input() comment?: string
+export class RatingCardComponent implements OnInit {
+  @Input() rating!: RatingViewDto
+  public userList$: Observable<ProfilListViewDto[]> = new Observable<ProfilListViewDto[]>()
+  public ratingUserMap$: Observable<Record<string, ProfilListViewDto>> = new Observable<Record<string, ProfilListViewDto>>()
+
+  constructor(private profileService: ProfileService) { }
+
+  ngOnInit(): void {
+    this.profileService.loadAll().subscribe()
+    this.userList$ = this.profileService.profilessShort$
+    this.ratingUserMap$ = this.userList$.pipe(
+      map(users => Object.fromEntries(users.map(user => [user.id, user])))
+    )
+  }
+
 }
